5233ee3cd3cbe0c3193aa9b516843c7296e8010f
Thien <tranngocthien628@gmail.com>
2025-05-20 01:15:40 +0700

    build: initialize project

---
+++ src/test/java/com/swpgroup01/SWP391_SmokingCessationSupportPlatform/Swp391SmokingCessationSupportPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swpgroup01.SWP391_SmokingCessationSupportPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391SmokingCessationSupportPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
