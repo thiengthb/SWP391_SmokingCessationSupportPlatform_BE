5c021da62c42ec13666d5ae67d12905938d6cd1c
thi3n <136480142+thiengthb@users.noreply.github.com>
2025-05-26 11:34:27 +0700

    build: initialize springboot project

---
+++ src/main/java/com/swpteam/smoking_cessation/Swp391SmokingCessationSupportPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.swpteam.smoking_cessation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391SmokingCessationSupportPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391SmokingCessationSupportPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swpteam/smoking_cessation/Swp391SmokingCessationSupportPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swpteam.smoking_cessation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391SmokingCessationSupportPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
