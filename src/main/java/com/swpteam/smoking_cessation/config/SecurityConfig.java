04b289ca1d2647774d8e3a15980bb0b16a3912d4
chính <loner9h@gmail.com>
2025-05-30 01:06:16 +0700

    feat: allow public endpoint

---
+++ src/main/java/com/swpteam/smoking_cessation/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.swpteam.smoking_cessation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Cho phép tất cả endpoint công khai
+                )
+                .csrf(csrf -> csrf.disable()); // Tắt CSRF để test API
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Để mã hóa mật khẩu
+    }
+}
