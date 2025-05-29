62df8eb2f4d80783a61924704187dd46d133d0e3
VuAnhTuan1805 <tuanmeh123@gmail.com>
2025-05-29 08:19:57 +0700

    feat(auth): Add Google and JWT authentication

---
+++ src/main/java/com/swpteam/smoking_cessation/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.swpteam.smoking_cessation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/oauth2/callback", "/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                ).csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
