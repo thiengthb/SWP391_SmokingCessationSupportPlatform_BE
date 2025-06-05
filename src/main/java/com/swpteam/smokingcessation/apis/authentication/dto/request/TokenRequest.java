960622abcd87c20ddceb29f8c6d2c4c2aff55251
VuAnhTuan1805 <tuanmeh123@gmail.com>
2025-06-06 01:02:36 +0700

    feat(valid): add validations for requests, move refresh token to response header

--- src/main/java/com/swpteam/smokingcessation/apis/authentication/dto/request/TokenRequest.java
+++ src/main/java/com/swpteam/smokingcessation/apis/authentication/dto/request/TokenRequest.java
@@ -8,6 +8,12 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
+<<<<<<<< HEAD:src/main/java/com/swpteam/smokingcessation/apis/authentication/dto/request/TokenRequest.java
 public class TokenRequest {
     String token;
+========
+public class TokenRefreshRequest {
+    @NotBlank(message = "REFRESH_TOKEN_REQUIRED")
+    String refreshToken;
+>>>>>>>> db0efc0 (feat(valid): add validations for requests, move refresh token to response header):src/main/java/com/swpteam/smokingcessation/apis/authentication/dto/request/TokenRefreshRequest.java
 }
