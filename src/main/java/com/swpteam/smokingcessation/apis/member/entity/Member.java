e8f26f5509a7b98e5348681473dc25510586b025
VuAnhTuan1805 <tuanmeh123@gmail.com>
2025-06-07 00:05:40 +0700

    feat(member): add member creation endpoint and map account_id to member

---
+++ src/main/java/com/swpteam/smokingcessation/apis/member/entity/Member.java
@@ -0,0 +1,41 @@
+package com.swpteam.smokingcessation.apis.member.entity;
+
+import com.swpteam.smokingcessation.apis.account.entity.Account;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    Account account;
+
+    @Column(name = "full_name")
+    String fullName;
+
+    LocalDate dob;
+    String address;
+    @Enumerated(EnumType.STRING)
+    MemberGender gender;
+    int score;
+
+    @Column(name = "current_streak")
+    int currentStreak;
+    @Column(name = "last_counter_reset")
+    LocalDateTime lastCounterReset;
+}
