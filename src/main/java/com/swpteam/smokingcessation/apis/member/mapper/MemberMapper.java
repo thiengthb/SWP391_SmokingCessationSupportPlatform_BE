e99fe0a2080f30f2c9de5d8ffd6ef7e147ddc896
VuAnhTuan1805 <tuanmeh123@gmail.com>
2025-06-08 13:51:06 +0700

    feat(member): add CRU for member

--- src/main/java/com/swpteam/smokingcessation/apis/member/mapper/MemberMapper.java
+++ src/main/java/com/swpteam/smokingcessation/apis/member/mapper/MemberMapper.java
@@ -1,10 +1,12 @@
 package com.swpteam.smokingcessation.apis.member.mapper;
 
 import com.swpteam.smokingcessation.apis.member.dto.request.MemberCreateRequest;
+import com.swpteam.smokingcessation.apis.member.dto.request.MemberUpdateRequest;
 import com.swpteam.smokingcessation.apis.member.dto.response.MemberResponse;
 import com.swpteam.smokingcessation.apis.member.entity.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
@@ -18,4 +20,5 @@ public interface MemberMapper {
     @Mapping(target = "accountId", source = "account.id")
     MemberResponse toMemberResponse(Member entity);
 
+    void updateMember(@MappingTarget Member entity, MemberUpdateRequest request);
 }
