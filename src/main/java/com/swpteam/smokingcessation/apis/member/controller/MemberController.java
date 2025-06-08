ccacf90009ad1f2d2c5ca44b4558a5d219d8110b
VuAnhTuan1805 <tuanmeh123@gmail.com>
2025-06-08 13:51:06 +0700

    feat(member): add CRU for member

--- src/main/java/com/swpteam/smokingcessation/apis/member/controller/MemberController.java
+++ src/main/java/com/swpteam/smokingcessation/apis/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.swpteam.smokingcessation.apis.member.controller;
 
 import com.swpteam.smokingcessation.apis.member.dto.request.MemberCreateRequest;
+import com.swpteam.smokingcessation.apis.member.dto.request.MemberUpdateRequest;
 import com.swpteam.smokingcessation.apis.member.dto.response.MemberResponse;
+import com.swpteam.smokingcessation.apis.member.entity.Member;
 import com.swpteam.smokingcessation.apis.member.service.MemberService;
 import com.swpteam.smokingcessation.common.response.ApiResponse;
 import jakarta.validation.Valid;
@@ -9,6 +11,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/member")
@@ -19,12 +23,33 @@ public class MemberController {
 
     @PostMapping("/create/{accountId}")
     ApiResponse<MemberResponse> createMember(@PathVariable("accountId") String accountId, @RequestBody @Valid MemberCreateRequest request) {
-        var result = memberService.createMember(accountId,request);
+        var result = memberService.createMember(request, accountId);
 
         return ApiResponse.<MemberResponse>builder()
                 .result(result)
                 .build();
     }
 
+    @GetMapping
+    List<Member> getMembers() {
+        return memberService.getMembers();
+    }
 
+    @GetMapping("/{accountId}")
+    ApiResponse<MemberResponse> getMemberById(@PathVariable("accountId") String id) {
+        var result = memberService.getMemberById(id);
+
+        return ApiResponse.<MemberResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PutMapping("/update/{accountId}")
+    ApiResponse<MemberResponse> updateAccount(@PathVariable("accountId") String id, @RequestBody MemberUpdateRequest request) {
+        var result = memberService.updateMember(request, id);
+
+        return ApiResponse.<MemberResponse>builder()
+                .result(result)
+                .build();
+    }
 }
--- src/main/java/com/swpteam/smokingcessation/apis/member/service/MemberService.java
+++ src/main/java/com/swpteam/smokingcessation/apis/member/service/MemberService.java
@@ -3,16 +3,20 @@ package com.swpteam.smokingcessation.apis.member.service;
 import com.swpteam.smokingcessation.apis.account.entity.Account;
 import com.swpteam.smokingcessation.apis.account.repository.AccountRepository;
 import com.swpteam.smokingcessation.apis.member.dto.request.MemberCreateRequest;
+import com.swpteam.smokingcessation.apis.member.dto.request.MemberUpdateRequest;
 import com.swpteam.smokingcessation.apis.member.dto.response.MemberResponse;
 import com.swpteam.smokingcessation.apis.member.entity.Member;
 import com.swpteam.smokingcessation.apis.member.mapper.MemberMapper;
 import com.swpteam.smokingcessation.apis.member.repository.MemberRepository;
 import com.swpteam.smokingcessation.exception.AppException;
 import com.swpteam.smokingcessation.exception.ErrorCode;
+import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -23,7 +27,7 @@ public class MemberService {
     MemberRepository memberRepository;
     AccountRepository accountRepository;
 
-    public MemberResponse createMember(String id, MemberCreateRequest request) {
+    public MemberResponse createMember(MemberCreateRequest request, String id) {
         Account account = accountRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.ACCOUNT_NOT_EXISTED));
 
 
@@ -37,4 +41,31 @@ public class MemberService {
         return memberMapper.toMemberResponse(memberRepository.save(member));
     }
 
+
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
+
+    public MemberResponse getMemberById(String id) {
+        return memberMapper.toMemberResponse(findMemberById(id));
+    }
+
+    private Member findMemberById(String id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.MEMBER_NOT_EXISTED));
+        if (member.getAccount().isDeleted()) {
+            throw new AppException(ErrorCode.ACCOUNT_DELETED);
+        }
+        return member;
+    }
+
+    @Transactional
+    public MemberResponse updateMember(MemberUpdateRequest request, String id) {
+        Member member = findMemberById(id);
+
+        memberMapper.updateMember(member, request);
+        memberRepository.save(member);
+
+        return memberMapper.toMemberResponse(member);
+    }
 }
