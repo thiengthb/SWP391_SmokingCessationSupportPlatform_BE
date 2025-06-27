91a93ecf30212af7610fa3e3027221de6c672708
VuAnhTuan1805 <tuanmeh123@gmail.com>
2025-06-27 23:32:26 +0700

    feat(counter): add counter endpoint for the counter method

---
+++ src/main/java/com/swpteam/smokingcessation/controller/v1/tracking/CounterController.java
@@ -0,0 +1,42 @@
+package com.swpteam.smokingcessation.controller.v1.tracking;
+
+import com.swpteam.smokingcessation.common.ApiResponse;
+import com.swpteam.smokingcessation.constant.SuccessCode;
+import com.swpteam.smokingcessation.domain.dto.counter.CounterResponse;
+import com.swpteam.smokingcessation.service.interfaces.tracking.ICounterService;
+import com.swpteam.smokingcessation.utils.ResponseUtil;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/counters")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Tag(name = "Counter", description = "Manage counter-related operations")
+public class CounterController {
+
+    ICounterService counterService;
+
+    @PutMapping("/start")
+    ResponseEntity<ApiResponse<CounterResponse>> startOrResetCounter() {
+        return ResponseUtil.buildResponse(
+                SuccessCode.COUNTER_START,
+                counterService.startCounter()
+        );
+    }
+
+    @GetMapping
+    ResponseEntity<ApiResponse<CounterResponse>> getCounter() {
+        return ResponseUtil.buildResponse(
+                SuccessCode.COUNTER_GET,
+                counterService.getCounter()
+        );
+    }
+}
---
+++ src/main/java/com/swpteam/smokingcessation/domain/dto/counter/CounterResponse.java
@@ -0,0 +1,17 @@
+package com.swpteam.smokingcessation.domain.dto.counter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CounterResponse {
+    LocalDateTime lastCounterReset;
+}
