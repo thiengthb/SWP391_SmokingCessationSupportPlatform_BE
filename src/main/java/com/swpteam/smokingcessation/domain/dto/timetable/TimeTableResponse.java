cd0944da251b9f7b7bde86dffc911bf818a30c62
ChinhTN <chinhtrannhatn@gmail.com>
2025-07-05 02:17:08 +0700

    refactor(booking): fix name in timetable and caching timetable auto

--- src/main/java/com/swpteam/smokingcessation/domain/dto/timetable/TimeTableResponse.java
+++ src/main/java/com/swpteam/smokingcessation/domain/dto/timetable/TimeTableResponse.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TimeTableResponse {
-
+        String name;
         String id;
         String coachId;
         String description;
