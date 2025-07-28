76b6bff884e48dbec059dcd322200437eea66c18
VuAnhTuan1805 <tuanmeh123@gmail.com>
2025-07-29 06:21:44 +0700

    chore: add username mappings for bookings

--- src/main/java/com/swpteam/smokingcessation/domain/mapper/BookingMapper.java
+++ src/main/java/com/swpteam/smokingcessation/domain/mapper/BookingMapper.java
@@ -12,6 +12,8 @@ public interface BookingMapper {
 
     @Mapping(source = "member.id", target = "memberId")
     @Mapping(source = "coach.id", target = "coachId")
+    @Mapping(source = "coach.coach.fullName", target = "coachFullName")
+    @Mapping(source = "member.username", target = "memberName")
     BookingResponse toResponse(Booking booking);
 
     Booking toEntity(BookingRequest bookingRequest);
